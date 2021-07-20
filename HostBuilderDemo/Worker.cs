1549a98152ed0144dc914a68456ab3868015eb57
pmann84 <peterdmann28@gmail.com>
2021-07-20 13:08:17 +0100

    Finishing touches

--- HostBuilderDemo/Worker.cs
+++ HostBuilderDemo/Worker.cs
@@ -10,17 +10,22 @@ namespace HostBuilderDemo
    class Worker : BackgroundService
    {
       private readonly ILogger<Worker> _logger;
+      private readonly IHostApplicationLifetime _applicationLifetime;
       private readonly Settings _settings;
       private readonly WorkerSettings _workerSettings;
 
       public Worker(
          ILogger<Worker> logger,
+         IHostApplicationLifetime applicationLifetime,
          IOptions<Settings> settings,
          IOptions<WorkerSettings> workerSettings)
       {
          _logger = logger;
+         _applicationLifetime = applicationLifetime;
          _settings = settings.Value;
          _workerSettings = workerSettings.Value;
+
+         Environment.ExitCode = 0;
       }
 
       public override Task StartAsync(CancellationToken cancellationToken)
@@ -37,15 +42,33 @@ namespace HostBuilderDemo
          return base.StopAsync(cancellationToken);
       }
 
+      private async Task DoWork(CancellationToken cancellationToken)
+      {
+         for (var i = 0; i < _workerSettings.Repetitions; i++)
+         {
+            _logger.LogInformation($"[{i + 1}] {_settings.OutputMessage}");
+         }
+      }
+
       protected override async Task ExecuteAsync(CancellationToken cancellationToken)
       {
          // Run background task
-         _logger.LogInformation("Running background task...");
-         for (var i = 0; i < _workerSettings.Repetitions; i++)
+         try
          {
-            _logger.LogInformation($"[{i+1}] {_settings.OutputMessage}");
+            _logger.LogInformation("Running background task...");
+            await DoWork(cancellationToken);
+            _logger.LogInformation("Finished background task...");
          }
-         _logger.LogInformation("Finished background task...");
+         catch (Exception e)
+         {
+            _logger.LogError($"Error occured: {e.Message}");
+            Environment.ExitCode = 1;
+         }
+         finally
+         {
+            _logger.LogInformation("Exiting application...");
+            _applicationLifetime.StopApplication();
+         }
       }
    }
 }

241e69ce7c9fd079be7d849fda4c9d7779f8d3d9
pmann84 <peterdmann28@gmail.com>
2021-07-20 12:04:14 +0100

    Setup structured logging and launch settings

--- HostBuilderDemo/Program.cs
+++ HostBuilderDemo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Compact;
 
 namespace HostBuilderDemo
 {
@@ -12,7 +14,7 @@ namespace HostBuilderDemo
 
       private static IHostBuilder CreateHostBuilder(string[] args)
       {
-         return Host.CreateDefaultBuilder(args);
+         return Host.CreateDefaultBuilder(args)
             //.ConfigureAppConfiguration((hostContext, config) =>
             //{
             //   // By default reads arguments from the following in order
@@ -22,11 +24,26 @@ namespace HostBuilderDemo
             //   // appsettings.{Environment}
             //   // You can do this manually or tweak this by specifying them explicitly with the following
             //   // you can also tweak the order by specifyng these in a different order too!
-            //   config.AddEnvironmentVariables("_");
+            //   config.AddEnvironmentVariables("HOSTBUILDERDEMO_");
             //   config.AddCommandLine(args);
-            //   config.AddJsonFile("appsettings.json");
+            //   config.AddJsonFile("appsettings.json", optional: true);
             //   config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true);
-            //});
+            //})
+            .UseSerilog((context, config) =>
+            {
+               config.ReadFrom.Configuration(context.Configuration);
+
+               if (context.Configuration["UseHumanLoggingFormat"]?.ToLower() == "true")
+               {
+                  const string outputTemplate =
+                     "[{Timestamp:dd/MM/yy HH:mm:ss.fff}][{Level:u3}] {Message:lj} {Properties}{NewLine}{Exception}";
+                  config.WriteTo.Async(sinkConfig => sinkConfig.Console(outputTemplate: outputTemplate));
+               }
+               else
+               {
+                  config.WriteTo.Async(sinkConfig => sinkConfig.Console(new RenderedCompactJsonFormatter()));
+               }
+            });
       }
    }
 }

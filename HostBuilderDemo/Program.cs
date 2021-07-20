53b147d7c77fb68608f378d4d0c03346e4da0266
pmann84 <peterdmann28@gmail.com>
2021-07-20 07:05:33 +0100

    Initial app setup

---
+++ HostBuilderDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace HostBuilderDemo
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         CreateHostBuilder(args).Build().Run();
+      }
+
+      private static IHostBuilder CreateHostBuilder(string[] args)
+      {
+         return Host.CreateDefaultBuilder(args);
+      }
+   }
+}

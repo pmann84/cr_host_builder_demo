2fa855a2a714bba580fc2390b875a7f6f71185ad
pmann84 <peterdmann28@gmail.com>
2021-07-20 10:16:53 +0100

    Added appsettings, launch settings and example of setting configuration

--- HostBuilderDemo/Program.cs
+++ HostBuilderDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace HostBuilderDemo
 {
@@ -12,6 +13,20 @@ namespace HostBuilderDemo
       private static IHostBuilder CreateHostBuilder(string[] args)
       {
          return Host.CreateDefaultBuilder(args);
+            //.ConfigureAppConfiguration((hostContext, config) =>
+            //{
+            //   // By default reads arguments from the following in order
+            //   // Environment variables - setting names prefixed with `DOTNET_`
+            //   // Command line arguments
+            //   // appsettings.json
+            //   // appsettings.{Environment}
+            //   // You can do this manually or tweak this by specifying them explicitly with the following
+            //   // you can also tweak the order by specifyng these in a different order too!
+            //   config.AddEnvironmentVariables("_");
+            //   config.AddCommandLine(args);
+            //   config.AddJsonFile("appsettings.json");
+            //   config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true);
+            //});
       }
    }
 }
